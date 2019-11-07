5055f805313a8293d73ba0df3204f78d69ac84bc
Gryveslol <57505753+Gryveslol@users.noreply.github.com>
2019-11-07 21:55:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var gameData = {
+  love: 0,
+  lovePerClick: 1
+  lovePerClickCost: 10
+}
+function patWaifu() {
+  gameData.love += gameData.lovePerClick
+  document.getElementById("loveGained").innerHTML = gameData.love + " Love Gained"
+}
+function buyLovePerClick() {
+  if (gameData.love) >= gameData.lovePerClickCost) {
+    gameData.love -= gameData.lovePerClickCost
+    gameData.lovePerClick += 1
+    gameData.lovePerClickCost *= 2
+    document.getElementById("loveGained").innerHTML = gameData.love + " Love Gained"
+    document.getElementById("perClickUpgrade").innerHTML = "Improve Technique (Currently Level " + gameData.lovePerClick + ") Cost: " + gameData.lovePerClickCost + " Love"
+  }
+}
+
+var mainGameLoop = window.setInterval(function() {
+  patWaifu()
+}, 1000)
+
+var saveGameLoop = window.setInterval(function() {
+  localStorage.setItem('idleWaifuSave', JSON.stringify(gameData))
+}, 15000)
+
+var savegame = JSON.parse(localStorage.getItem("idleWaifuSave"))
+if (savegame !== null) {
+  gameData = savegame
+}
