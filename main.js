29ccb35277a375e573e0d9ed9c7d70555dc4ee4d
Gryveslol <57505753+Gryveslol@users.noreply.github.com>
2019-11-07 22:57:03 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ var gameData = {
   lovePerClick: 1,
   lovePerClickCost: 10
 }
-function patWaifu() {
+
+function getLove() {
   gameData.love += gameData.lovePerClick
   document.getElementById("loveGained").innerHTML = gameData.love + " Love Gained"
 }
+
 function buyLovePerClick() {
-  if (gameData.love) >= gameData.lovePerClickCost) {
+  if (gameData.love >= gameData.lovePerClickCost) {
     gameData.love -= gameData.lovePerClickCost
     gameData.lovePerClick += 1
     gameData.lovePerClickCost *= 2
@@ -18,14 +20,14 @@ function buyLovePerClick() {
 }
 
 var mainGameLoop = window.setInterval(function() {
-  patWaifu()
+  getLove()
 }, 1000)
-
-var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem('idleWaifuSave', JSON.stringify(gameData))
-}, 15000)
 
 var savegame = JSON.parse(localStorage.getItem("idleWaifuSave"))
 if (savegame !== null) {
   gameData = savegame
 }
+
+var saveGameLoop = window.setInterval(function() {
+  localStorage.setItem('idleWaifuSave', JSON.stringify(gameData))
+}, 15000)
